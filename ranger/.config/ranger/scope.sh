0793d32ac7cfc49a1454f6b43cef085d674ca26e
alex-ryzhkov <junkntrash@tutanota.com>
2020-11-05 16:56:42 +0300

    Fix memory leak problem in rangerwhen opening files containing images

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -95,9 +95,13 @@ handle_image() {
         #     exit 1;;
 
         # Image
-        image/*)
-            local orientation
-            orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
+        # https://github.com/ranger/ranger/issues/1815
+        # image/*)
+        #     local orientation
+        #     orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
+        image/png | image/jpeg)
+        local orientation
+        orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
             # If orientation data is present and the image actually
             # needs rotating ("1" means no rotation)...
             if [[ -n "$orientation" && "$orientation" != 1 ]]; then
